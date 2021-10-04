d1b1c3426288dbe6d838f0302b5fc9beb0ec2ff8
samuelswandi <16520414@std.stei.itb.ac.id>
2021-10-05 00:33:41 +0700

    Adding 2 button above video player

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <iframe src="Video.mp4" allowfullscreen></iframe>
+      <div className="choiceButton">
+        <button className="button1">
+          Mie Kuah
+        </button>
+        <button className="button2">
+          Mie Goreng
+        </button>
+      </div>
+      <iframe className="videoPlayer" src="Video.mp4" allowfullscreen></iframe>
     </div>
   );
 }
